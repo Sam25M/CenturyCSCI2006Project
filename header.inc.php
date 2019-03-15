8ac6cac0f63c014268bd914085e5afbc38ea4d20
Shelby Medlock <43891819+Sam25M@users.noreply.github.com>
2019-03-15 11:59:52 -0500

    Add files via upload

---
+++ header.inc.php
@@ -0,0 +1,11 @@
+<header>
+  <nav id="headnav">
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="logIn.html">My Account</a></li>
+      <li><a href="accountReg.html">Account Registration</a></li>
+      <li><a href="cart.html">Cart</a></li>
+    </ul>
+    <h1>College Bookstore</h1>
+  </nav>
+</header>
