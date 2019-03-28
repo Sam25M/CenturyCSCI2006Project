1c3ecc0f7ae7d9f8c1053827ef3b7fc34ef39785
Shelby Medlock <43891819+Sam25M@users.noreply.github.com>
2019-03-28 13:54:48 -0500

    Add files via upload

--- studentMarketplace.php
+++ studentMarketplace.php
@@ -4,12 +4,14 @@
 		<meta charset="utf-8">
 		<title>Student Marketplace</title>
 		<link href="css/mainStyles.css" rel="stylesheet"/>
-		<script src="js/index.js" type="text/javascript"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	</head>
 	<body>
 		<?php include "includes/header.inc.php";?>
-
+		<main>
+      <p>This page will display books that students have added to sell. Can be filtered based on category (CSCI, MATH, etc.). Use browse-paintings.php from wa2 as example. Should connect to
+			bookPage.php and send the selected books isbn, bookPage.php?isbn= , to bookPage.php.</p>
+		</main>
 		<?php include "includes/footer.inc.php";?>
 	</body>
 </html>
