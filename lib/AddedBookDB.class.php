cf9f9e272337c380ba51228925d66500efb3df2f
Shelby Medlock <43891819+Sam25M@users.noreply.github.com>
2019-03-28 14:04:28 -0500

    Add files via upload

---
+++ lib/AddedBookDB.class.php
@@ -0,0 +1,25 @@
+<?php
+  //Used to take books from marketbooks table for the student marketplace.
+  class AddedBookDB{
+    private $pdo = null;
+
+    private static $baseSQL = "SELECT * FROM MarketBooks";
+    private static $constraint = " order by title";
+
+    public function __construct($connection) {
+        $this->pdo = $connection;
+    }
+
+    public function findById($id){
+      $sql = self::$baseSQL.' WHERE isbn=?';
+      $statement = DatabaseHelper::runQuery($this->pdo, $sql, Array($id));
+      return $statement->fetch();
+    }
+
+    public function getAll(){
+      $sql = self::$baseSQL.self::$constraint;
+      $statement = DatabaseHelper::runQuery($this->pdo, $sql, null);
+      return $statement;
+    }
+  }
+?>
