b0a293fd402dc1b7a859748e7a5d06a119b2656d
Shelby Medlock <43891819+Sam25M@users.noreply.github.com>
2019-03-28 13:53:05 -0500

    Add files via upload

--- bookPage.php
+++ bookPage.php
@@ -1,40 +1,42 @@
 <?php
 	include "includes/config.inc.php";
 
-	$subjectId = null;
+	$subjectId = null; //for school market side.
+	$bookIsbn = null; //for student market side.
 	if(isset($_GET['id'])){
-		$subjectId = $_GET['id'];
+		//school market side.
+		include "includes/bookPageSchool.inc.php";
 	}
 
-	$books = new SchoolBookDB($pdo);
-	$book = $books->findById($subjectId);
-	$subjects = new SubjectDB($pdo);
-	$subject = $subjects->findById($subjectId);
-	$subjectTitle = $subject['title'];
-
-	$title = $book['title'];
-	$newPrice = $book['newprice'];
-	$usedPrice = $book['usedprice'];
-	$img = $book['bookCover'];
-	$author = $book['author'];
-	$edition = $book['edition'];
-	$pubDate = $book['pubDate'];
-	$isbn = $book['isbn'];
-	$text = $book['description'];
-
+	if(isset($_GET['isbn'])){
+		//student market side.
+		include "includes/bookPageStudent.inc.php";
+	}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title><?php echo $subjectTitle; ?></title>
+		<title><?php if ($subjectId != null) {
+			echo $subjectTitle;
+		}
+		if ($bookIsbn != null) {
+			echo "Student Marketplace";
+		}
+		?></title>
 		<link href="css/mainStyles.css" rel="stylesheet"/>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	</head>
 	<body>
 		<?php include "includes/header.inc.php";?>
 		<article class="book">
-			<h2><?php echo $subjectTitle; ?></h2>
+			<h2><?php if ($subjectId != null) {
+				echo $subjectTitle;
+			}
+			if ($bookIsbn != null) {
+				echo $title;
+			}
+			?></h2>
 			<figure class="bookImg">
 				<img src="images/<?php echo $img; ?>.jpg" alt="<?php echo $title; ?>" height="15%" width="15%">
 			</figure>
@@ -49,14 +51,24 @@
 						<td><strong>Title</strong></td>
 						<td id="bTitle"><?php echo $title; ?></td>
 					</tr>
-					<tr>
-						<td><strong>Price (New)</strong></td>
-						<td id="bNewPrice">$<?php echo $newPrice; ?></td>
-					</tr>
-					<tr>
-						<td><strong>Price (Used)</strong></td>
-						<td id="bUsedPrice">$<?php echo $usedPrice; ?></td>
-					</tr>
+					<?php
+						if($subjectId != null){
+							echo "<tr>
+								<td><strong>Price (New)</strong></td>
+								<td id=\"bNewPrice\">$".$newPrice."</td>
+							</tr>
+							<tr>
+								<td><strong>Price (Used)</strong></td>
+								<td id=\"bUsedPrice\">$".$usedPrice."</td>
+							</tr>";
+						}
+						if($bookIsbn != null){
+							echo "<tr>
+								<td><strong>Price</strong></td>
+								<td id=\"bPrice\">$".$price."</td>
+							</tr>";
+						}
+					?>
 					<tr>
 						<td><strong>Author</strong></td>
 						<td><?php echo $author; ?></td>
@@ -65,22 +77,45 @@
 						<td><strong>Edition/Copyright</strong></td>
 						<td><?php echo $edition; ?></td>
 					</tr>
-					<tr>
-						<td><strong>Published Date</strong></td>
-						<td><?php echo $pubDate; ?></td>
-					</tr>
+					<?php
+						if ($subjectId != null) {
+							echo "<tr>
+								<td><strong>Published Date</strong></td>
+								<td>".$pubDate."</td>
+							</tr>";
+						}
+						if ($bookIsbn != null) {
+							echo "<tr>
+								<td><strong>Published Date</strong></td>
+								<td>".$quality."</td>
+							</tr>";
+						}
+					?>
 					<tr>
 						<td><strong>ISBN</strong></td>
 						<td><?php echo $isbn; ?></td>
 					</tr>
 				</tbody>
 			</table>
-			<input type="button" id="newButton" value="Add New to Cart">
-			<input type="button" id="usedButton" value="Add Used to Cart">
-			<section>
-				<h3>Textbook Description</h3>
-				<p><?php echo $text; ?></P>
-			</section>
+			<form method="post" action="cart.php">
+			<?php
+				if ($subjectId != null) {
+					echo "<input type=\"submit\" id=\"newButton\" value=\"Add New to Cart\">
+					<input type=\"submit\" id=\"usedButton\" value=\"Add Used to Cart\">";
+				}
+				if ($bookIsbn != null) {
+					echo "<input type=\"submit\" id=\"addCartButton\" value=\"Add to Cart\"";
+				}
+			?>
+			</form>
+			<?php
+				if($subjectId != null){
+					echo "<section>
+						<h3>Textbook Description</h3>
+						<p>".$text."</P>
+					</section>";
+				}
+			?>
 		</article>
 		<?php include "includes/footer.inc.php";?>
 		<script src="js/bookPages.js" type="text/javascript"></script>
