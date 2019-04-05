c4d8ff00945f4740de82b216e0923016715660d2
Shelby Medlock <43891819+Sam25M@users.noreply.github.com>
2019-04-05 10:21:08 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
-<?php 
-	include "includes/config.inc.php";
-?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
 		<title>College Bookstore</title>
 		<link href="css/mainStyles.css" rel="stylesheet"/>
-		<script src="js/index.js" type="text/javascript"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	</head>
 	<body>
 		<?php include "includes/header.inc.php";?>
-
-		<a href="instructorList.php">School Marketplace</a>
-		<a href="studentMarketplace.php">Student Marketplace</a>
-
+		<form>
+			<input type="button" value="School Marketplace" id="schoolBtn" onclick="location.href='instructorList.php';"/>
+			<input type="button" value="Student Marketplace" id="studentBtn" onclick="location.href='studentMarketplace.php';"/>
+			<!--School Marketplace button will go to instructorList.php. Student Marketplace button will
+		go to studentMarketplace.php -->
+		</form>
 		<img class="library" src="images/library.jpg" alt="library">
 
 		<?php include "includes/footer.inc.php";?>
