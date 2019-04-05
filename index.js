931d6c9bce5f38133b9758f8e921ec32361144c8
Shelby Medlock <43891819+Sam25M@users.noreply.github.com>
2019-04-05 10:02:33 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// slightly sloppy way of disabling/enabling textbox
+function unhide(){
+  document.getElementById("otherbox").disabled = false;
+}
+
+function hide(){
+  document.getElementById("otherbox").disabled = true;
+}
