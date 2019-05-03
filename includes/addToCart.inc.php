ac13048079ae6f1640ec054f6617c252c315a131
Shelby Medlock <43891819+Sam25M@users.noreply.github.com>
2019-05-03 16:14:08 -0500

    Add files via upload

--- includes/addToCart.inc.php
+++ includes/addToCart.inc.php
@@ -2,24 +2,20 @@
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if (isset($_POST['newButton'])) {
-      //setcookie for name=title value=isbn
       $inCart = true;
       setcookie($subjectId, $isbn, time()+60*60*24);
       setcookie($isbn, 'newBook', time()+60*60*24);
     }
 
     if (isset($_POST['usedButton'])) {
-      //setcookie for name=title value=isbn
       $inCart = true;
       setcookie($subjectId, $isbn, time()+60*60*24);
       setcookie($isbn, 'usedBook', time()+60*60*24);
     }
 
     if(isset($_POST['addCartButton'])){
-      //setcookie for name=postId value=$postId
       $postInCart = true;
       setcookie($postId, $isbn, time()+60*60*24);
-      //setcookie($isbn, $postId, time()+60*60*24);
     }
 
   }
