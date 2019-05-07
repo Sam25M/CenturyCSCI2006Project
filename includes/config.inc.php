1a08d9c5d4d41383a27f2aa3d5a63f329231fac5
Shelby Medlock <43891819+Sam25M@users.noreply.github.com>
2019-05-06 19:30:31 -0500

    Add files via upload

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -9,7 +9,7 @@ spl_autoload_register(function ($class) {
 try{
   define('DBCONNECTION', 'mysql:host=localhost;dbname=csci2006project');
   define('DBUSER', 'root');
-  define('DBPASS', '');
+  define('DBPASS', 'root');
   $pdo = new PDO(DBCONNECTION, DBUSER, DBPASS);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch (PDOException $e){
--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,1 +1,1 @@
-<footer>CSCI 2006 Project; Spring 2019; Baani; By: Shelby Medlock and Tom McDonald</footer>
+<footer class="w3-container">CSCI 2006 Project; Spring 2019; Baani; By: Shelby Medlock and Tom McDonald</footer>
